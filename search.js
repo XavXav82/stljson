e8abc81eaac511f82b918750f5a261db75f9ce68
XavXav82 <151407766+XavXav82@users.noreply.github.com>
2024-02-08 12:50:39 +1100

    Update search.js

--- search.js
+++ search.js
@@ -1,2 +1,3 @@
-var jsonFile = require("grades.json");
+fetch("./grades.json").then((response) => response.json()).then((json) => console.log(json));
+//var jsonFile = ("grades.json");
 console.log(jsonFile);
