84f8884e003028a2a79d5029f77125b182771c4d
XavXav82 <151407766+XavXav82@users.noreply.github.com>
2024-02-08 21:39:32 +1100

    Update search.js

--- search.js
+++ search.js
@@ -72,7 +72,7 @@ function getAllUrlParams(url) {
   let tempName = ""
   for(let i = 1;i<12400;i++){
     tempName = json[String(i)]["name"];
-    if(tempName.search(searchVal != -1){
+    if(tempName.search(searchVal != -1)){
       window.location.replace("https://link.stleonards.vic.edu.au/eportfolio/"+String(i)+"/profile");
     }
   }
